ab96893034af8bfe09af54cd01bd273e87918d3a
Ava-TU <C24384981@myTUDublin.ie>
2025-05-15 12:50:35 +0100

    creating scripts for enemy and spawner

---
+++ 2025_test_c24384981/Scenes/enemy.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var speed = 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _physics_process(delta: float) -> void:
+	var movement = Vector2(20, speed)
